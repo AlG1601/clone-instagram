7a1237b80eaf1a65e1b13b2777f6d441db86365b
Guilherme Alefe de Sousa dos Santos <92280255+AlG1601@users.noreply.github.com>
2023-09-28 12:32:29 -0300

    comite feito pelo brian

--- src/Componentes/ConteudoEsquerdo.jsx
+++ src/Componentes/ConteudoEsquerdo.jsx
@@ -1,7 +1,15 @@
 import ItemMenuLateralEsquerda from "./ItemMenuLateralEsquerda"
 import { AiFillHome } from "react-icons/ai";
 import { HiMagnifyingGlass } from "react-icons/hi2";
-import { MdOutlineExplore } from "react-icons/md";
+import { MdOutlineExplore, MdWidthNormal } from "react-icons/md";
+import { BiBorderAll, BiMoviePlay } from "react-icons/bi";
+import { TbSend } from "react-icons/tb";
+import { FaRegHeart } from "react-icons/fa";
+import { CgAddR } from "react-icons/cg";
+import imgPerfil from "../img/gab.jpeg";
+import logo from "../img/logo-ins.png";
+
+var icons = {'font-size': "24px"};
 export default function ConteudoEsquerdo () {
 
     return (
@@ -10,11 +18,15 @@ export default function ConteudoEsquerdo () {
         <div>
             <div className="menu">
                 <ul>
-                    <ItemMenuLateralEsquerda icone={<AiFillHome />} texto="Home" />
-                    <ItemMenuLateralEsquerda icone={<HiMagnifyingGlass />} texto="Busca" />
-                    <ItemMenuLateralEsquerda icone={<MdOutlineExplore />} texto="Explorar" />
-                    <ItemMenuLateralEsquerda icone="Interoog" texto="Exemplo" />
-                    <li>Menu</li>
+                    <ItemMenuLateralEsquerda icone={<img src={logo} width={"103px"} />} />
+                    <ItemMenuLateralEsquerda icone={<AiFillHome style={icons} />} texto="Home" />
+                    <ItemMenuLateralEsquerda icone={<HiMagnifyingGlass style={icons} />} texto="Busca" />
+                    <ItemMenuLateralEsquerda icone={<MdOutlineExplore style={icons} />} texto="Explorar" />
+                    <ItemMenuLateralEsquerda icone={<BiMoviePlay style={icons} />} texto="Reels" />
+                    <ItemMenuLateralEsquerda icone={<TbSend style={icons} />} texto="Mensagens" />
+                    <ItemMenuLateralEsquerda icone={<FaRegHeart style={icons} />} texto="Notificações" />
+                    <ItemMenuLateralEsquerda icone={<CgAddR style={icons} />} texto="Criar" />
+                    <ItemMenuLateralEsquerda icone={<img src={imgPerfil} style={{'width': "24px", 'borderRadius': '12px'}} />} texto="Perfil" />
                 </ul>
             </div>
         </div>
